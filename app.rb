6e74d116f47fe48a2453eb2041e3ff50ea5435ae
kalex19 <klewis@rockymountainprep.org>
2018-07-17 14:12:51 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -14,6 +14,12 @@ class App < Sinatra::Base
   end
 
   # Code your final two routes here:
+all_the_medicines = [
+  #<Medicine:0x007fb739b1af88 @id=1, @name="penicillin" @group="antibiotic">,
+  #<Medicine:0x007fb739b1af88 @id=2, @name="advil" @group="anti-inflammatory">,
+  #<Medicine:0x007fb739b1af88 @id=3, @name="benadryl" @group="anti-histamine">
+]
+
 get '/medicines/:id' do
   @medicine = all_the_medicines.select do |medicine|
     medicine.id == params[:id]
